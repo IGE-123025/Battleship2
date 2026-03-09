f81ce61a1f9c02ae99d8786bf605c60d39ad28fa
Dinis Oliveira <droao2@iscte-iul.pt>
2026-03-09 18:00:39 +0000

    create PdfExportService class

--- src/main/java/battleship/Main.java
+++ src/main/java/battleship/Main.java
@@ -3,6 +3,9 @@
  */
 package battleship;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The type Main.
  *
@@ -19,6 +22,18 @@ public class Main
 	 */
 	public static void main(String[] args)
     {
+
+		List<Jogada> jogadas = new ArrayList<>();
+
+		jogadas.add(new Jogada(1, "Jogador 1", "moveu para A3"));
+		jogadas.add(new Jogada(2, "Jogador 2", "atacou B4"));
+		jogadas.add(new Jogada(3, "Jogador 1", "defendeu C2"));
+
+		PdfExport pdfExportService = new PdfExport();
+
+		pdfExportService.exportarJogadasParaPDF(jogadas, "jogadas.pdf");
+
+		System.out.println("PDF gerado com sucesso.");
 		System.out.println("***  Battleship  ***");
 
 		Tasks.menu();
---
+++ src/main/java/battleship/PdfExport.java
@@ -0,0 +1,46 @@
+package battleship;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+
+import java.io.IOException;
+import java.util.List;
+
+
+public class PdfExport {
+    public void exportarJogadasParaPDF(List<Jogada> jogadas, String nomeFicheiro) {
+        try (PDDocument document = new PDDocument()) {
+            PDPage page = new PDPage();
+            document.addPage(page);
+
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+
+            contentStream.beginText();
+            contentStream.setFont(PDType1Font.HELVETICA_BOLD, 16);
+            contentStream.newLineAtOffset(50, 750);
+            contentStream.showText("Historico de Jogadas");
+            contentStream.endText();
+
+            float y = 720;
+
+            for (Jogada jogada : jogadas) {
+                String linha = jogada.getNumero() + " - " + jogada.getJogador() + ": " + jogada.getDescricao();
+
+                contentStream.beginText();
+                contentStream.setFont(PDType1Font.HELVETICA, 12);
+                contentStream.newLineAtOffset(50, y);
+                contentStream.showText(linha);
+                contentStream.endText();
+
+                y -= 20;
+            }
+
+            contentStream.close();
+            document.save(nomeFicheiro);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
